1ae06ffdd87b22d5940f793afa102b066ce68943
Frank Mitchell <me@frankmitchell.org>
2018-08-19 16:58:16 -0400

    Define rules for adding crew to the ship.

--- js/singularity.js
+++ js/singularity.js
@@ -19,6 +19,14 @@ Ship.create = () => {
   };
 };
 
+Ship.clone = ship => JSON.parse(JSON.stringify(ship));
+
+Ship.set = (ship, tile, value) => {
+  const copy = Ship.clone(ship);
+  copy.layout[tile] = value;
+  return copy;
+};
+
 const Renderer = {};
 
 Renderer.render = (ship) => {
@@ -33,6 +41,64 @@ Renderer.render = (ship) => {
 
 Renderer.invalidate = (ship) => {
   requestAnimationFrame(() => Renderer.render(ship));
+};
+
+const Rules = {};
+
+Rules.isCenter = (ship, tile) => {
+  const file = tile.slice(0, 1);
+  const rank = tile.slice(-1);
+
+  const centerFiles = ship.files.slice(1, -1);
+  const centerRanks = ship.ranks.slice(1, -1);
+
+  return centerFiles.indexOf(file) > -1 && centerRanks.indexOf(rank) > -1;
+};
+
+Rules.isEdge = (ship, tile) => {
+  return !Rules.isCenter(ship, tile);
+};
+
+Rules.isCorner = (ship, tile) => {
+  const file = tile.slice(0, 1);
+  const rank = tile.slice(-1);
+
+  const firstFile = ship.files.slice(0, 1)[0];
+  const lastFile = ship.files.slice(-1)[0];
+
+  const firstRank = ship.ranks.slice(0, 1)[0];
+  const lastRank = ship.ranks.slice(-1)[0];
+
+  return (file === firstFile || file === lastFile) && (rank === firstRank || rank === lastRank);
+};
+
+Rules.canAddCrew = (ship, tile) => {
+  // Crew can only be added to an empty space on the ship.
+  if (ship.layout[tile] !== '') {
+    return false;
+  }
+
+  // There are a max of four crew on the ship.
+  const crew = Object.keys(ship.layout).filter(id => ship.layout[id] === 'crew');
+  if (crew.length >= 4) {
+    return false;
+  }
+
+  // Crew can not go in a corner.
+  if (Rules.isCorner(ship, tile)) {
+    return false;
+  }
+
+  // Crew must go on an edge.
+  return Rules.isEdge(ship, tile);
+};
+
+Rules.addCrew = (ship, tile) => {
+  if (Rules.canAddCrew(ship, tile)) {
+    return Ship.set(ship, tile, 'crew');
+  }
+
+  return Ship.clone(ship);
 };
 
 (function game() {
