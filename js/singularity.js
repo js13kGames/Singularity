3d65ffdbb008608fbb8ef63a9bc63271ffc5618f
Frank Mitchell <me@frankmitchell.org>
2018-08-19 15:01:27 -0400

    Render the ship when the game first loads.

--- js/singularity.js
+++ js/singularity.js
@@ -19,14 +19,69 @@ Ship.create = () => {
   };
 };
 
+const Renderer = {};
+
+Renderer.render = (ship) => {
+  const $ = window.jQuery;
+
+  Object.keys(ship.layout).forEach((id) => {
+    const element = $(`#${id}`);
+    element.removeClass('crew');
+    element.addClass(ship.layout[id]);
+  });
+};
+
+Renderer.invalidate = (ship) => {
+  requestAnimationFrame(() => Renderer.render(ship));
+};
+
 (function game() {
+  let ship = Ship.create();
+
   function reset() {
-    Ship.create();
+    ship = Ship.create();
   }
 
   function play() {
     reset();
+    Renderer.invalidate(ship);
   }
 
   window.onload = play;
+}());
+
+(function $() {
+  function Fn(selector) {
+    if (selector instanceof Fn) {
+      return selector;
+    }
+
+    this.element = selector;
+
+    if (typeof selector === 'string') {
+      if (selector.indexOf(0) === '#') {
+        this.element = document.getElementById(selector.slice(1));
+      }
+    }
+
+    return this;
+  }
+
+  Fn.prototype.addClass = function addClass(klass) {
+    if (this.element && this.element.classList && klass) {
+      this.element.classList.add(klass);
+    }
+  };
+
+  Fn.prototype.removeClass = function removeClass(klass) {
+    if (this.element && this.element.classList) {
+      this.element.classList.remove(klass);
+    }
+  };
+
+  function root(selector) {
+    return new Fn(selector);
+  }
+
+  window.jQuery = root;
 }());
