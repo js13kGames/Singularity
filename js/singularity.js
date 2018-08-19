5275f9309802e13d3f0d642d53ff85786b992310
Frank Mitchell <me@frankmitchell.org>
2018-08-19 13:23:31 -0400

    Create a new ship when the game starts.

--- js/singularity.js
+++ js/singularity.js
@@ -1,1 +1,32 @@
 const Ship = {};
+
+Ship.create = () => {
+  const files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  const ranks = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+  const layout = {};
+
+  files.forEach((file) => {
+    ranks.forEach((rank) => {
+      layout[file + rank] = '';
+    });
+  });
+
+  return {
+    files,
+    ranks,
+    layout,
+  };
+};
+
+(function game() {
+  function reset() {
+    Ship.create();
+  }
+
+  function play() {
+    reset();
+  }
+
+  window.onload = play;
+}());
