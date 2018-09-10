56dd0b11c31eb796e2cf6cfb54cc57458023d964
Frank Mitchell <me@frankmitchell.org>
2018-09-09 21:23:37 -0400

    Fuzz 2,000,000 game boards. Still a 98% win rate.

--- fuzz.js
+++ fuzz.js
@@ -2,27 +2,28 @@
 
 const { Ship, Rules, AI } = require('./js/singularity');
 
-const results = {};
-let ship;
-
 let max = parseInt(process.argv[2], 10);
 
 if (isNaN(max)) {
   max = 1;
 }
 
-for (let i = 0; i < max; i += 1) {
-  const ship = AI.create();
-  Object.keys(ship.layout).forEach((id) => {
-    if (ship.layout[id] === '') {
-      ship.layout[id] = 'junction';
-    }
-  });
-  const rescued = AI.rescued(ship).length;
-  if (results[rescued] === undefined) {
-    results[rescued] = 0;
-  }
-  results[rescued] += 1;
-}
+while (true) {
+  const results = {};
 
-console.log('Rescued', results);
+  for (let i = 0; i < max; i += 1) {
+    const ship = AI.create();
+    Object.keys(ship.layout).forEach((id) => {
+      if (ship.layout[id] === '') {
+        ship.layout[id] = 'junction';
+      }
+    });
+    const rescued = AI.rescued(ship).length;
+    if (results[rescued] === undefined) {
+      results[rescued] = 0;
+    }
+    results[rescued] += 1;
+  }
+
+  console.log('Rescued', results);
+}
